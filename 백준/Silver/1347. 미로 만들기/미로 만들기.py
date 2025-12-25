85a773347d4fdd67336541d89f5125b6cbc5f2f1
Godel99 <tylor9632@gmail.com>
2025-12-25 21:42:28 +0900

    [Silver II] Title: 미로 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1347. 미로 만들기/미로 만들기.py
+++ 백준/Silver/1347. 미로 만들기/미로 만들기.py
@@ -4,35 +4,35 @@ input=sys.stdin.readline
 
 def main():
     n = int(input())
-    note = input().strip()
+    moves = input()
 
-    x = y = d = 0
-    dx = [-1, 0, 1, 0]
-    dy = [0, -1, 0, 1]
+    # 방향: 남(0), 동(1), 북(2), 서(3)
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1]
 
+    x, y, d = 0, 0, 0  # 시작 위치와 방향(남쪽)
     visited = {(0, 0)}
 
-    for c in note:
-        if c == 'R':
-            d = (d + 1) % 4
-        elif c == 'L':
-            d = (d + 3) % 4
-        else:
-            x += dx[d]; y += dy[d]
+    for move in moves:
+        if move == 'F':
+            x += dx[d]
+            y += dy[d]
             visited.add((x, y))
+        elif move == 'L':
+            d = (d - 1) % 4
+        else:  # 'R'
+            d = (d + 1) % 4
 
+    # 최소 직사각형 범위 계산
+    min_x = min(pos[0] for pos in visited)
+    max_x = max(pos[0] for pos in visited)
+    min_y = min(pos[1] for pos in visited)
+    max_y = max(pos[1] for pos in visited)
 
-    xs, ys = zip(*visited)
-    minx, maxx = min(xs), max(xs)
-    miny, maxy = min(ys), max(ys)
-
-    maze = [['#'] * (maxy - miny + 1) for _ in range(maxx - minx + 1)]
-
-    for x, y in visited:
-        maze[x-minx][y-miny] = '.'
-    
-    for row in maze[::-1]:
-        sys.stdout.write(''.join(row)+'\n')
+    # 미로 생성
+    for i in range(min_x, max_x + 1):
+        print(''.join('.' if (i, j) in visited else '#' 
+                    for j in range(max_y, min_y - 1, -1)))
 
 if __name__ == '__main__':
     main()
