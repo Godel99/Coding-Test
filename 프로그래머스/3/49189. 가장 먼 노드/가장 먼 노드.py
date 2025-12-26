3d5d9a076c5e308b44c4ec70513349d87918b2ec
Godel99 <tylor9632@gmail.com>
2025-12-26 14:00:35 +0900

    [level 3] Title: 가장 먼 노드, Time: 28.09 ms, Memory: 20 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,23 @@
+from collections import defaultdict, deque
+
+def solution(n, edge):
+    edges = defaultdict(list)
+    for a,b in edge:
+        edges[a].append(b)
+        edges[b].append(a)
+
+    distance = [-1] * (n + 1)
+    distance[1] = 0
+    
+    queue = deque([1])
+
+    while queue:
+        nd = queue.popleft()
+        for e in edges[nd]:
+            if distance[e] < 0:
+                queue.append(e)
+                distance[e] = distance[nd] + 1
+
+    max_distance = max(distance)
+
+    return distance.count(max_distance)
