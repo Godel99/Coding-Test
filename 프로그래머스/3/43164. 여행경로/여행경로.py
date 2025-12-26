781038ff30d3c97f7981c881f34c28695e9e4ad6
Godel99 <tylor9632@gmail.com>
2025-12-26 23:31:05 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -2,30 +2,54 @@ from collections import defaultdict, deque
 
 def solution(tickets):
     graph = defaultdict(list)
+    
+    # 그래프 구성
     for d, a in tickets:
         graph[d].append(a)
-
+    
+    # 사전순 경로를 위해 역순 정렬
     for g in graph:
-        graph[g].sort()
+        graph[g].sort(reverse=True)
+    
+    stack = ['ICN']
+    trace = []
 
-    trace = ['ICN']
+    while stack:
+        cur = stack[-1]
+        if graph[cur]:
+            stack.append(graph[cur].pop())
+        else:
+            trace.append(stack.pop())
 
-    def DFS(cur):
-        if len(trace) == len(tickets) + 1:
-            return True
+    return trace[::-1]
 
-        for i, a in enumerate(graph[cur]):
-            graph[cur].pop(i)
-            trace.append(a)
+
+# def solution(tickets):
+#     graph = defaultdict(list)
+#     for d, a in tickets:
+#         graph[d].append(a)
+
+#     for g in graph:
+#         graph[g].sort()
+
+#     trace = ['ICN']
+
+#     def DFS(cur):
+#         if len(trace) == len(tickets) + 1:
+#             return True
+
+#         for i, a in enumerate(graph[cur]):
+#             graph[cur].pop(i)
+#             trace.append(a)
         
-            if DFS(a):
-                return True
+#             if DFS(a):
+#                 return True
             
-            trace.pop()
-            graph[cur].insert(i, a)
+#             trace.pop()
+#             graph[cur].insert(i, a)
 
-        return False
+#         return False
 
-    DFS('ICN')
+#     DFS('ICN')
                 
-    return trace
+#     return trace
