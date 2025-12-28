44cf1ca8dbe9b0a93fbe911a411929b4a1e6075a
Godel99 <tylor9632@gmail.com>
2025-12-28 21:10:03 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 514.79 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -1,18 +1,34 @@
 def solution(sequence):
-    A = []
-    B = []
-    n = len(sequence)
+    cur1 = cur2 = 0
+    ans1 = ans2 = 0
+
     for i, v in enumerate(sequence):
-        if i % 2:
-            A.append(-v)
-            B.append(v)
-        else:
-            A.append(v)
-            B.append(-v)
+        a = v if i%2 else -v
+        b = -a
 
-    for i in range(1, n):
-        A[i] = max(A[i], A[i-1]+A[i])
-        B[i] = max(B[i], B[i-1]+B[i])
+        cur1 = max(a, cur1 + a)
+        cur2 = max(b, cur2 + b)
 
-    return max(max(A), max(B))
+        ans1 = max(ans1, cur1)
+        ans2 = max(ans2, cur2)
+
+    return max(ans1, ans2)
+
+# def solution(sequence):
+#     A = []
+#     B = []
+#     n = len(sequence)
+#     for i, v in enumerate(sequence):
+#         if i % 2:
+#             A.append(-v)
+#             B.append(v)
+#         else:
+#             A.append(v)
+#             B.append(-v)
+
+#     for i in range(1, n):
+#         A[i] = max(A[i], A[i-1]+A[i])
+#         B[i] = max(B[i], B[i-1]+B[i])
+
+#     return max(max(A), max(B))
     
