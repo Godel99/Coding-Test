21cb6516645240913d03855de154676fa5ab9284
Godel99 <tylor9632@gmail.com>
2025-12-29 19:33:14 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 5.73 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -2,47 +2,79 @@ import heapq
 
 def solution(board):
     n = len(board)
-    INF = float('inf')
+    INF = 10**9
+    dp = [[[INF]*4 for _ in range(n)] for _ in range(n)]
     
-    # dp[r][c][dir] : (r,c)에 dir 방향으로 도착했을 때 최소 비용
-    dp = [[[INF] * 4 for _ in range(n)] for _ in range(n)]
-    
-    # 방향: 상, 우, 하, 좌
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
-    
+    dirs = [(-1,0), (0,1), (1,0), (0,-1)]
     pq = []
     
-    # 시작점에서 가능한 두 방향만 초기화
-    if board[0][1] == 0:
-        dp[0][1][1] = 100
-        heapq.heappush(pq, (100, 0, 1, 1))
-    if board[1][0] == 0:
-        dp[1][0][2] = 100
-        heapq.heappush(pq, (100, 1, 0, 2))
+    for dir, (nr, nc) in enumerate([(0,1), (1,0)]):
+        if board[nr][nc] == 0:
+            dp[nr][nc][dir+1] = 100
+            heapq.heappush(pq, (100, nr, nc, dir+1))
     
     while pq:
         cost, r, c, dir = heapq.heappop(pq)
-        
         if cost > dp[r][c][dir]:
             continue
         
-        for ndir in range(4):
-            nr = r + dx[ndir]
-            nc = c + dy[ndir]
-            
-            if nr < 0 or nr >= n or nc < 0 or nc >= n:
-                continue
-            if board[nr][nc] == 1:
+        for ndir, (dr, dc) in enumerate(dirs):
+            nr, nc = r + dr, c + dc
+            if not (0 <= nr < n and 0 <= nc < n) or board[nr][nc]:
                 continue
             
             ncost = cost + (100 if ndir == dir else 600)
-            
             if ncost < dp[nr][nc][ndir]:
                 dp[nr][nc][ndir] = ncost
                 heapq.heappush(pq, (ncost, nr, nc, ndir))
     
     return min(dp[n-1][n-1])
+
+# import heapq
+
+# def solution(board):
+#     n = len(board)
+#     INF = float('inf')
+    
+#     # dp[r][c][dir] : (r,c)에 dir 방향으로 도착했을 때 최소 비용
+#     dp = [[[INF] * 4 for _ in range(n)] for _ in range(n)]
+    
+#     # 방향: 상, 우, 하, 좌
+#     dx = [-1, 0, 1, 0]
+#     dy = [0, 1, 0, -1]
+    
+#     pq = []
+    
+#     # 시작점에서 가능한 두 방향만 초기화
+#     if board[0][1] == 0:
+#         dp[0][1][1] = 100
+#         heapq.heappush(pq, (100, 0, 1, 1))
+#     if board[1][0] == 0:
+#         dp[1][0][2] = 100
+#         heapq.heappush(pq, (100, 1, 0, 2))
+    
+#     while pq:
+#         cost, r, c, dir = heapq.heappop(pq)
+        
+#         if cost > dp[r][c][dir]:
+#             continue
+        
+#         for ndir in range(4):
+#             nr = r + dx[ndir]
+#             nc = c + dy[ndir]
+            
+#             if nr < 0 or nr >= n or nc < 0 or nc >= n:
+#                 continue
+#             if board[nr][nc] == 1:
+#                 continue
+            
+#             ncost = cost + (100 if ndir == dir else 600)
+            
+#             if ncost < dp[nr][nc][ndir]:
+#                 dp[nr][nc][ndir] = ncost
+#                 heapq.heappush(pq, (ncost, nr, nc, ndir))
+    
+#     return min(dp[n-1][n-1])
 
 # def solution(board):
 #     n = len(board)
