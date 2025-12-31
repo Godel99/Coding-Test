248c0aea2d0ee655e26a950a2aa310a771a05e7b
Godel99 <tylor9632@gmail.com>
2025-12-31 15:36:07 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 188.01 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -1,24 +1,42 @@
 def solution(s):
     n = len(s)
-    length = 1
-    idx = 0
+    def expand(left, right):
+        while left >= 0 and right < n and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1
+    
+    max_len = 1
+    for i in range(n):
+        odd_len = expand(i, i)
+        even_len = expand(i, i+1)
 
-    for idx in range(n):
-        if idx + 1 < n and s[idx] == s[idx+1]:
-            left = idx
-            right = idx + 1
-            while left >= 0 and right < n and s[left] == s[right]:
-                left -= 1
-                right += 1
-            length = max(length, (right - left - 1))
+        max_len = max(max_len, odd_len, even_len)
 
-        if idx - 1 >= 0 and idx + 1 < n and s[idx-1] == s[idx+1]:
-            left = idx - 1
-            right = idx + 1
-            while left >= 0 and right < n and s[left] == s[right]:
-                left -= 1
-                right += 1
-            length = max(length, (right - left - 1))
+    return max_len
+
+
+# def solution(s):
+#     n = len(s)
+#     length = 1
+#     idx = 0
+
+#     for idx in range(n):
+#         if idx + 1 < n and s[idx] == s[idx+1]:
+#             left = idx
+#             right = idx + 1
+#             while left >= 0 and right < n and s[left] == s[right]:
+#                 left -= 1
+#                 right += 1
+#             length = max(length, (right - left - 1))
+
+#         if idx - 1 >= 0 and idx + 1 < n and s[idx-1] == s[idx+1]:
+#             left = idx - 1
+#             right = idx + 1
+#             while left >= 0 and right < n and s[left] == s[right]:
+#                 left -= 1
+#                 right += 1
+#             length = max(length, (right - left - 1))
         
-    return length
+#     return length
         
