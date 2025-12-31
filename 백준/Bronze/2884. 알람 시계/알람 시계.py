9b6126d5be1d4d53aae0ebad8cd2b3e4f0469512
Godel99 <tylor9632@gmail.com>
2025-12-31 17:03:04 +0900

    [Bronze III] Title: 알람 시계, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -0,0 +1,16 @@
+def main():
+    H, M = map(int, input().split())
+    
+    if M >= 45:
+        M -= 45
+    else:
+        if H == 0:
+            H = 23
+        else:
+            H -= 1
+        M = 60 - (45 - M)
+    
+    print(f'{H} {M}')
+
+if __name__ == '__main__':
+    main()
