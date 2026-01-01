4e2e06d065229860ce4477898718ac382e2c0bda
Godel99 <tylor9632@gmail.com>
2026-01-01 22:15:23 +0900

    [Bronze II] Title: 음계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[8];
+    int asc = 0, desc = 0;
+
+    for(int i = 0; i < 8; i++){
+        cin >> arr[i];
+        if(arr[i] == i + 1) asc++;
+        if(arr[i] == 8 - i) desc++;
+    }
+
+    if (asc == 8) cout << "ascending";
+    else if (desc == 8) cout << "descending";
+    else cout << "mixed";
+}
