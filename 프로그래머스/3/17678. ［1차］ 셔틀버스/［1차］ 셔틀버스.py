14cdd8c977c6731c148bcc1a16fdcf6fc38fdc04
Godel99 <tylor9632@gmail.com>
2026-01-02 13:59:41 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 1.10 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -0,0 +1,31 @@
+def solution(n, t, m, timetable):
+    crew = []
+    for time in timetable:
+        hour, min = map(int, time.split(':'))
+        crew.append(hour*60+min)
+    crew.sort()
+    crew_idx = 0
+    last_ridden = 0
+    last_time = 0
+    
+    for i in range(n):
+        last_bus_time= 9 * 60 + (t * i)
+        ridden = 0
+
+        while crew_idx < len(crew) and crew[crew_idx] <= last_bus_time and ridden < m:
+            crew_idx += 1
+            ridden += 1
+        
+        if i == n - 1:
+            last_ridden = ridden
+            if ridden > 0:
+                last_time = crew[crew_idx-1]
+
+    if last_ridden < m:
+        result_time = last_bus_time 
+    else:
+        result_time = max(0, last_time - 1)
+
+    H = result_time // 60
+    M = result_time % 60
+    return f'{H:02d}:{M:02d}'
