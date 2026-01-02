a5aa609fc0a1c10b28e8044523bd9d9674a28a68
Godel99 <tylor9632@gmail.com>
2026-01-02 18:20:25 +0900

    [level 3] Title: 거스름돈, Time: 409.17 ms, Memory: 12.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -0,0 +1,10 @@
+def solution(n, money):
+    MOD = 1000000007
+    dp = [0] * (n + 1)
+    dp[0] = 1
+
+    for coin in money:
+        for x in range(coin, n+1):
+            dp[x] += dp[x-coin]
+    
+    return dp[n] % MOD
