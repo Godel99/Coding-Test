b1b7c9f585a7242bebc78a91fc896e2a95e71b54
Godel99 <tylor9632@gmail.com>
2026-01-04 19:19:03 +0900

    [level 3] Title: 풍선 터트리기, Time: 208.08 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -1,23 +1,44 @@
 def solution(a):
     n = len(a)
-    if n < 3:
-        cnt = n
-    else:
-        cnt = 2
+    if n <= 2:
+        return n
+    
+    surive = set()
+    cur_min = float('inf')
+    for x in a:
+        if x < cur_min:
+            cur_min = x
+            surive.add(x)
 
-    right_min = [0] * n
-    right_min[n-1] = a[n-1]
+    cur_min = float('inf')
+    for x in reversed(a):
+        if x < cur_min:
+            cur_min = x
+            surive.add(x)
+    
+    return len(surive)
 
-    Lmin = a[0]
-    for i in range(n-2, 0, -1):
-        right_min[i] = min(right_min[i+1], a[i+1])
 
-    for i in range(1, n-1):
-        Lmin = min(Lmin, a[i-1])
+# def solution(a):
+#     n = len(a)
+#     if n < 3:
+#         cnt = n
+#     else:
+#         cnt = 2
 
-        if a[i] > Lmin and a[i] > right_min[i]:
-            continue
-        else:
-            cnt += 1
+#     right_min = [0] * n
+#     right_min[n-1] = a[n-1]
 
-    return cnt
+#     Lmin = a[0]
+#     for i in range(n-2, 0, -1):
+#         right_min[i] = min(right_min[i+1], a[i+1])
+
+#     for i in range(1, n-1):
+#         Lmin = min(Lmin, a[i-1])
+
+#         if a[i] > Lmin and a[i] > right_min[i]:
+#             continue
+#         else:
+#             cnt += 1
+
+#     return cnt
