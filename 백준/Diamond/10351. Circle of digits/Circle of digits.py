dccdb93b6f7f83a8c8ca570a4f2e27521f20bc9a
Godel99 <tylor9632@gmail.com>
2026-01-07 10:58:17 +0900

    [Diamond IV] Title: Circle of digits, Time: 8476 ms, Memory: 71300 KB -BaekjoonHub

---
+++ 백준/Diamond/10351. Circle of digits/Circle of digits.py
@@ -0,0 +1,72 @@
+import sys
+def print(*args, sep=" ", end="\n"):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+def input():
+    return sys.stdin.readline().rstrip()
+
+def SA(s):
+    n = len(s); m = max(n, 1024)+1
+    sa = list(range(n)); nr = [0] * (2*n); idx = [0] * n
+    r = [0] * (2*n)
+    for i in range(n):
+        r[i] = int(s[i]) + 1
+
+    d = 1
+    while d < n:
+        cmp = lambda i, j: (
+            r[i] < r[j] or (r[i] == r[j] and r[i+d] < r[j+d])
+        )
+        cnt = [0] * m
+        for i in range(n): 
+            val = r[i+d] if i+d < n else 0
+            cnt[val] += 1
+        for i in range(1, m): cnt[i] += cnt[i-1]
+        for i in range(n-1, -1, -1): 
+            val = r[i+d] if i+d < n else 0
+            cnt[val] -= 1
+            idx[cnt[val]] = i
+        cnt = [0] * m
+        for i in range(n): cnt[r[i]] += 1
+        for i in range(1, m): cnt[i] += cnt[i-1]
+        for i in range(n-1, -1, -1): 
+            cnt[r[idx[i]]] -= 1
+            sa[cnt[r[idx[i]]]] = idx[i]
+        nr[sa[0]] = 1
+        for i in range(1, n): nr[sa[i]] = nr[sa[i-1]] + cmp(sa[i-1], sa[i])
+        for i in range(n): r[i] = nr[i]
+        d <<= 1
+    global rev, ret
+    rev = [0] * (n//2); ret = [0] * (n//2); ii = 0
+    for i in sa:
+        if i < n//2:
+            rev[i] = ii
+            ret[ii] = i; ii +=1
+
+def main():
+    n, k = map(int, input().split())
+    s = input(); s = s + s
+    if(n==k):
+        ans = 0
+        for i in range(n): ans = max(ans, int(s[i]))
+        return print(ans)
+    SA(s)
+    l = 0; r = len(ret) - 1
+    while l < r:
+        mid = l+r>>1
+        flag = False
+        for x in range((n-1)//k+1):
+            cnt = 0; now = x
+            while now < x+n:
+                if rev[now%n] <= rev[ret[mid]]: now += 1
+                now += (n-1)//k
+                cnt += 1
+            if cnt <= k:
+                flag = True
+                break
+
+        if flag: r = mid
+        else: l = mid + 1
+    print(s[ret[l]: ret[l]+(n-1)//k+1])
+if __name__ == '__main__':
+    main()
+
---
+++ 백준/Diamond/10351. Circle of digits/README.md
@@ -0,0 +1,32 @@
+# [Diamond IV] Circle of digits - 10351 
+
+[문제 링크](https://www.acmicpc.net/problem/10351) 
+
+### 성능 요약
+
+메모리: 71300 KB, 시간: 8476 ms
+
+### 분류
+
+그리디 알고리즘, 문자열, 이분 탐색, 매개 변수 탐색, 접미사 배열과 LCP 배열
+
+### 제출 일자
+
+2026년 1월 7일 10:53:49
+
+### 문제 설명
+
+<p>You are given a circular string of length N that consists of digits '1'..'9'. You want to split it into K continuous non-empty parts. Each of those parts represents a decimal notation of some integer number. Your goal is to find a partition that minimizes the maximum integer from the partition at hand.</p>
+
+<p>For example, if the string is 7654321 and K=3 then the optimal partition is: {176, 54, 32} which has 176 as the maximum number. Note that the string is cyclic, that is the first character goes right after the last one (as in the 176 part of the above example).</p>
+
+### 입력 
+
+ <p>The first line of the input contains two integers N and K (3 ≤ N ≤ 100000, 2 ≤ K ≤ N). The second line contains a string of length N which consists only of characters ‘1’..’9’.</p>
+
+### 출력 
+
+ <p>Output the value of the maximal number in the optimal partition.</p>
+
+<p> </p>
+
