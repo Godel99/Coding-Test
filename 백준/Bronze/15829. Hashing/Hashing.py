91791dca2a31795dd64c0ef54d56795f59ea6a8e
Godel99 <tylor9632@gmail.com>
2026-01-10 17:15:26 +0900

    [Bronze II] Title: Hashing, Time: 48 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,16 @@
+import sys
+def print(*args, sep=" ", end="\n"):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+def input():
+    return sys.stdin.readline().rstrip()
+
+def main():
+    MOD = 1234567891
+    n = int(input())
+    s = input()
+    h = 0
+    for i, c in enumerate(s):
+        h += (ord(c) - ord('a') + 1) * pow(31, i, MOD)
+    print(h)
+if __name__ == '__main__':
+    main()
