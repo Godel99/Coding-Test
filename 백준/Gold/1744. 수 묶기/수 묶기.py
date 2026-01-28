05d850e2a38a88db5f0ad75db4aac68cb67cce07
Godel99 <tylor9632@gmail.com>
2026-01-28 21:21:43 +0900

    [Gold IV] Title: 수 묶기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.py
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -6,23 +6,20 @@ def main():
     n = int(input())
     pos = []
     neg = []
-    ones = zero = 0
+    ans = 0
     for _ in range(n):
         x = int(input())
-        if x > 1: pos.append(x)
-        elif x == 1: ones +=1
-        elif x == 0: zero = 1
-        else: neg.append(x)
+        if x <= 0: neg.append(x)
+        elif x > 1: pos.append(x)
+        else: ans += 1
     pos.sort(reverse=True)
     neg.sort()
-    ans = ones
     for i in range(0, len(pos), 2):
         if i + 1 < len(pos): ans += pos[i]*pos[i+1]
         else: ans += pos[i]
     for i in range(0, len(neg), 2):
         if i + 1 < len(neg): ans += neg[i]*neg[i+1]
-        else:  
-            if zero == 0: ans += neg[i]
+        else: ans += neg[i]
     print(ans)
     return 0
 if __name__ == '__main__':
