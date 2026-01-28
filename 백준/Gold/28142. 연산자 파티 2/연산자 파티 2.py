427bc199bf879034f85a84d1a9c076cedc9020d3
Godel99 <tylor9632@gmail.com>
2026-01-28 11:22:58 +0900

    [Gold III] Title: 연산자 파티 2, Time: 28 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/28142. 연산자 파티 2/연산자 파티 2.py
+++ 백준/Gold/28142. 연산자 파티 2/연산자 파티 2.py
@@ -1,28 +1,21 @@
 import sys
-
-input=sys.stdin.readline
+def print(*args, sep=" ", end="\n"): sys.stdout.write(sep.join(map(str, args)) + end)
+def input(): return sys.stdin.readline().rstrip()
 
 def main():
     p = 1000000007
     n = int(input())
     l = n - n % 255
     x = 0
-    
     for i in range(l, n+1):
         x -= i
         x = abs(x)
-        if i % 3 == 0: 
-            x = (x * i) % p
-        if i % 15 == 0:
-            x &= i
-        if i % 63 == 0:
-            x ^= i
-        if i % 255 == 0:
-            x |= i
-        if i % 1023 == 0:
-            x = (x * pow(2, i, p)) % p
-    
-    sys.stdout.write(str(x))
-
+        if i % 3 == 0: x = (x * i) % p
+        if i % 15 == 0: x &= i
+        if i % 63 == 0: x ^= i
+        if i % 255 == 0: x |= i
+        if i % 1023 == 0: x = (x * pow(2, i, p)) % p
+    print(x)
+    return 0
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
