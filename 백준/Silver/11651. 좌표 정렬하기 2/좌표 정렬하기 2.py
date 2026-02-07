f24f8002b7e8fc834b6326605b6298c5b311662d
Godel99 <tylor9632@gmail.com>
2026-02-07 10:25:49 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 284 ms, Memory: 55000 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,11 @@
+import sys
+def print(*args, sep=" ", end="\n"): return sys.stdout.write(sep.join(map(str, args)) + end)
+def input(): return sys.stdin.readline().rstrip()
+
+def main():
+    n = int(input())
+    A = sorted([tuple(map(int, input().split())) for _ in range(n)], key=lambda x: (x[1], x[0]))
+    for a in A: print(a[0], a[1])
+    return 0
+if __name__ == '__main__':
+    sys.exit(main())
