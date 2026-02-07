32bb84b4b9359adbf35617efea5f34c4cced20b5
Godel99 <tylor9632@gmail.com>
2026-02-07 10:19:20 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 296 ms, Memory: 47288 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -4,7 +4,7 @@ def input(): return sys.stdin.readline().rstrip()
 
 def main():
     n = int(input())
-    A = sorted([tuple(map(int, input().split())) for _ in range(n)], key=lambda x: (x[0], x[1]))
+    A = sorted([tuple(map(int, input().split())) for _ in range(n)])
     for a in A: print(a[0], a[1])
     return 0
 if __name__ == '__main__':
