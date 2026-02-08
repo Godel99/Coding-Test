7300f134edd3ce89afb4adcfc809e2a61badd687
Godel99 <tylor9632@gmail.com>
2026-02-08 19:09:10 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -4,15 +4,11 @@ def input(): return sys.stdin.readline().rstrip()
 
 def main():
     n = int(input())
-    ans = 0
-    while n >= 0:
-        if n % 5 == 0:
-            ans += (n//5)
-            print(ans)
-            break
-        n -= 3
-        ans += 1
-    else: print(-1)
+    if n == 4 or n == 7: return int(not print(-1))
+    q, r = divmod(n, 5)
+    if r == 0: print(q)
+    elif r == 1 or r == 3: print(q + 1)
+    elif r == 2 or r == 4: print(q + 2)
     return 0
 if __name__ == '__main__':
     sys.exit(main())
