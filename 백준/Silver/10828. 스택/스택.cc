d4019506463039403ceeec7bcd4735b271336f75
Godel99 <tylor9632@gmail.com>
2026-02-09 23:54:46 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -7,20 +7,20 @@ int main(){
     int n; cin >> n;
     stack<int> st;
     while(n--){
-        string com; cin >> com;
-        if(com == "push"){
+        string cmd; cin >> cmd;
+        if(cmd == "push"){
             int x; cin >> x;
             st.push(x);
         }
-        else if(com == "pop"){
+        else if(cmd == "pop"){
             if(st.size()){
                 cout << st.top() << '\n'; st.pop();
             }
             else cout << -1 << '\n';
         }
-        else if(com == "size") cout << st.size() << '\n';
-        else if(com == "empty") cout << (st.size() ? 0 : 1) << '\n';
-        else if(com == "top") cout << (st.size() ? st.top() : -1) << '\n';
+        else if(cmd == "size") cout << st.size() << '\n';
+        else if(cmd == "empty") cout << (st.size() ? 0 : 1) << '\n';
+        else if(cmd == "top") cout << (st.size() ? st.top() : -1) << '\n';
     }
     return 0;
 }
