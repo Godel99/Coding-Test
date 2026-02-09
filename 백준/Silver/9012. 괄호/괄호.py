142a3ba492544e61924252361af69459f3ce4748
Godel99 <tylor9632@gmail.com>
2026-02-09 18:44:51 +0900

    [Silver IV] Title: 괄호, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,20 @@
+import sys
+def print(*args, sep=" ", end="\n"): return sys.stdout.write(sep.join(map(str, args)) + end)
+def input(): return sys.stdin.readline().rstrip()
+#MOD = 1_000_000_007
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        s = input()
+        st = []
+        flag = 1
+        for c in s:
+            if c == '(': st.append(c)
+            elif st: st.pop()
+            else: flag = 0
+        if flag and not st: print('YES')
+        else: print('NO')
+    return 0
+if __name__ == '__main__':
+    sys.exit(main())
