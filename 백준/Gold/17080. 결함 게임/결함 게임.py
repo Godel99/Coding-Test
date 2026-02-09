ab5ac8a6a0c592988bf044aabbd5fc2421106eb5
Godel99 <tylor9632@gmail.com>
2026-02-09 21:07:34 +0900

    [Gold II] Title: 결함 게임, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17080. 결함 게임/결함 게임.py
+++ 백준/Gold/17080. 결함 게임/결함 게임.py
@@ -3,8 +3,7 @@ def print(*args, sep=" ", end="\n"): return sys.stdout.write(sep.join(map(str, a
 def input(): return sys.stdin.readline().rstrip()
 
 def main():
-    n = int(input())
-    print(2 if n&3 == 3 else 1)
+    print(2 if int(input())&3 == 3 else 1)
     return 0
 if __name__ == '__main__':
     sys.exit(main())
