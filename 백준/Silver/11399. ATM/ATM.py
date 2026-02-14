17bb99e14fa1f8851e4f398b9de79ca9167138b1
Godel99 <tylor9632@gmail.com>
2026-02-14 20:39:45 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,15 @@
+import sys
+def print(*args, sep=" ", end="\n"): return sys.stdout.write(sep.join(map(str, args)) + end)
+def input(): return sys.stdin.readline().rstrip()
+
+def main():
+    n = int(input())
+    p = list(map(int, input().split()))
+    p.sort()
+    ans = 0
+    for i in range(1, n+1):
+        ans += sum(p[:i])
+    print(ans)
+    return 0
+if __name__ == '__main__':
+    sys.exit(main())
