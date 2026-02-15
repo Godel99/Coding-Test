ab2074f7a450cf36d78cc01cc2a93cf6982df116
Godel99 <tylor9632@gmail.com>
2026-02-15 10:22:21 +0900

    [Gold I] Title: 언젠가 정렬이 될 수 있으면 좋겠네., Time: 428 ms, Memory: 28360 KB -BaekjoonHub

--- 백준/Gold/31003. 언젠가 정렬이 될 수 있으면 좋겠네．/언젠가 정렬이 될 수 있으면 좋겠네．.cc
+++ 백준/Gold/31003. 언젠가 정렬이 될 수 있으면 좋겠네．/언젠가 정렬이 될 수 있으면 좋겠네．.cc
@@ -2,6 +2,7 @@
 using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
+using min_heap = priority_queue<pii, vector<pii>, greater<pii>>;
 
 int main(){
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
@@ -17,13 +18,13 @@ int main(){
             }
         }
     }
-    priority_queue<pii> pq;
-    for(int i = 0; i < n; i++) if(!deg[i]) pq.push({-a[i], i});
+    min_heap pq;
+    for(int i = 0; i < n; i++) if(!deg[i]) pq.push({a[i], i});
     while(pq.size()){
-        int cur = pq.top().second; pq.pop();
-        cout << a[cur] << ' ';
+        auto [x, cur] = pq.top(); pq.pop();
+        cout << x << ' ';
         for(auto& nxt : e[cur]){
-            if(!(--deg[nxt])) pq.push({-a[nxt], nxt});
+            if(!(--deg[nxt])) pq.push({a[nxt], nxt});
         }
     }
     return 0;
