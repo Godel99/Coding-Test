99f97251560c4f17b4bad8a510ea83639579cdad
Godel99 <tylor9632@gmail.com>
2026-02-16 21:57:55 +0900

    [Silver III] Title: 계단 오르기, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -10,7 +10,7 @@ def main():
     dp = [0]*(n+1)
     dp[1] = score[1]
     dp[2] = score[1]+score[2]
-    dp[3] = max(score[1]+score[3], score[2]+score[3])
+    dp[3] = max(score[1], score[2])+score[3]
     for i in range(4, n+1): dp[i] = max(dp[i-3]+score[i-1], dp[i-2])+score[i]
     print(dp[n])
     return 0
