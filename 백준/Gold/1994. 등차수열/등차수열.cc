ce90745e03396a7fb854eb943a1088261b28fe42
Godel99 <tylor9632@gmail.com>
2026-02-17 14:43:38 +0900

    [Gold I] Title: 등차수열, Time: 80 ms, Memory: 18100 KB -BaekjoonHub

--- 백준/Gold/1994. 등차수열/등차수열.cc
+++ 백준/Gold/1994. 등차수열/등차수열.cc
@@ -1,22 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-using ll = long long;
+map<int, int> cnt;
+vector<int> v;
+int d[2020][2020];
+
+int dp(int i, int j){
+	int& ret = d[i][j];
+	if(ret) return ret;
+	ret = 2;
+	int nxt = lower_bound(v.begin()+j, v.end(), 2*v[j]-v[i]) - v.begin();
+	if(v[nxt] != 2*v[j]-v[i]) return ret;
+	return ret = 1 + dp(j, nxt);
+}
 
 int main(){
-    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-    int n, ans = 1; cin >> n;
-    vector<ll> a(n);
-    for(int i = 0; i < n; i++) cin >> a[i];
-    sort(a.begin(), a.end());
-    vector<unordered_map<ll, int>> dp(n);
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < i; j++){
-            ll d = a[i]-a[j];
-            if(dp[j].find(d)==dp[j].end()) dp[i][d] = 2;
-            else dp[i][d] = dp[j][d]+1;
-            ans = max(ans, dp[i][d]);  
-        }
-    }
-    cout << ans;
-    return 0;
+	cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
+	int n, ans = 2; cin >> n;
+	if(n == 1) return !(cout << 1);
+	for(int i = 0; i < n; i++){
+		int k; cin >> k;
+		if(cnt.find(k) != cnt.end()){
+			cnt[k]++;
+			ans = max(ans, cnt[k]);
+		}
+		else{
+			cnt.insert({k,1});
+			v.push_back(k);
+		}
+	}
+	sort(v.begin(), v.end());
+	for(int i = 0; i < v.size(); i++){
+		for(int j = i+1; j < v.size(); j++){
+			ans = max(ans, dp(i, j));
+		}
+	}
+	cout << ans;
 }
