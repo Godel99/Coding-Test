c84ccabf49ad3d817fda8802ed7e39d9a06d26c4
Godel99 <tylor9632@gmail.com>
2026-02-20 10:36:17 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+    int n; cin >> n;
+    if(n == 1) return !(cout << 1);
+    vector<int> dp(n+1, 0);
+    dp[1] = 1;
+    dp[2] = 3;
+    for(int i = 3; i <= n; i++) dp[i] = (dp[i-1]+dp[i-2]*2)%10007;
+    cout << dp[n];
+    return 0;
+}
