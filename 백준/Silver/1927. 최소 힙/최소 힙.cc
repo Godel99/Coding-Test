1f6439e8b0d540d5502cdee72ce04fa508a8e3ee
Godel99 <tylor9632@gmail.com>
2026-02-23 21:32:24 +0900

    [Silver II] Title: 최소 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(0);cout.tie(0);ios::sync_with_stdio(false);
+    int n; cin >> n;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    while(n--){
+        int x; cin >> x;
+        if(x){
+            pq.push(x);
+        }
+        else{
+            if(pq.size()){
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+            else cout << 0 << '\n';
+        }
+    }
+    return 0;
+}
