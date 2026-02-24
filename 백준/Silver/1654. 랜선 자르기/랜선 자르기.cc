ea248729be700df28b3407fac86d6d762c3c8697
Godel99 <tylor9632@gmail.com>
2026-02-24 18:27:44 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(0);cout.tie(0);ios::sync_with_stdio(false);
+    int k, n; cin >> k >> n;
+    vector<ll> line(k);
+    ll low = 1, high = 0;
+    for(int i = 0; i < k; i++){
+        cin >> line[i];
+        if(high < line[i]) high = line[i];
+    }
+    while(low <= high){
+        ll mid = (low+high)/2;
+        int total = 0;
+        for(auto l : line){
+            total += l/mid;
+        }
+        if(total >= n) low = mid+1;
+        else high = mid-1;
+    }
+    cout << high;
+    return 0;
+}
