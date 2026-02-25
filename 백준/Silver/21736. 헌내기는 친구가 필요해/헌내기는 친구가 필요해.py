997fdbb5649d23f2d364864bda171eb90a5ca91a
Godel99 <tylor9632@gmail.com>
2026-02-25 21:54:26 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 328 ms, Memory: 53152 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -6,23 +6,21 @@ dx = (1, -1, 0, 0); dy = (0, 0, 1, -1)
 
 def main():
     n, m = map(int, input().split())
-    board = [input() for _ in range(n)]
-    visited = [[False]*m for _ in range(n)]
+    board = [list(input()) for _ in range(n)]
     for i in range(n):
         for j in range(m):
             if board[i][j] == 'I': 
                 sx, sy = (i, j)
                 break
-    visited[sx][sy] = True
     st = [(sx, sy)]
     ans = 0
     while st:
         x, y = st.pop()
-        if board[x][y] == 'P': ans += 1
         for dir in range(4):
             nx = x+dx[dir]; ny = y+dy[dir]
-            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and board[nx][ny] != 'X':
-                visited[nx][ny] = True
+            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] != 'X':
+                if board[nx][ny] == 'P': ans += 1
+                board[nx][ny] = 'X'
                 st.append((nx, ny))
     if ans: print(ans)
     else: print('TT')
