7c2ee3e42d5ca00725d6ca94f7523528062ecb6c
Godel99 <tylor9632@gmail.com>
2026-02-25 22:17:37 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 12 ms, Memory: 3852 KB -BaekjoonHub

---
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+const int dx[] = {1, -1, 0, 0}, dy[] = {0, 0, 1, -1};
+
+int main(){
+    cin.tie(0);cout.tie(0);ios::sync_with_stdio(false);
+    int n, m; cin >> n >> m;
+    pii start;
+    vector<vector<char>> board(n, vector<char>(m));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> board[i][j];
+            if(board[i][j] == 'I'){
+                board[i][j] = 'X';
+                start.first = i, start.second = j;
+            }
+        }
+    }
+    stack<pii> st;
+    st.push(start);
+    int ans = 0;
+    while(st.size()){
+        auto[x, y] = st.top(); st.pop();
+        for(int dir = 0; dir < 4; dir++){
+            int nx = x+dx[dir], ny = y+dy[dir];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if(board[nx][ny] != 'X'){
+                if(board[nx][ny] == 'P') ans++;
+                board[nx][ny] = 'X';
+                st.push({nx, ny});
+            }
+        }
+    }
+    if(ans) cout << ans;
+    else cout << "TT";
+    return 0;
+}
