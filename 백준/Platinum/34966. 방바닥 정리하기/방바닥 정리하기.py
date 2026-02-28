b7c0e9e6b09080e73730baf206995ac414436202
Godel99 <tylor9632@gmail.com>
2026-02-28 10:39:07 +0900

    [Platinum V] Title: 방바닥 정리하기, Time: 752 ms, Memory: 190456 KB -BaekjoonHub

--- 백준/Platinum/34966. 방바닥 정리하기/방바닥 정리하기.py
+++ 백준/Platinum/34966. 방바닥 정리하기/방바닥 정리하기.py
@@ -26,21 +26,21 @@ def main():
         dp = [[inf]*n for _ in range(n)]
         dq[0].append(start)
         dp[ay][ax] = 0
-        now = 0
+        res = 0
         while any(dq):
-            while not dq[0]: dq[0], dq[1], dq[2] = dq[1], dq[2], deque(); now += 1
-            while dq[0]:
-                y, x = dq[0].popleft()
-                if dp[y][x] < now: continue
+            while not dq[res%3]: res += 1
+            while dq[res%3]:
+                y, x = dq[res%3].popleft()
+                if dp[y][x] < res: continue
                 for dir in range(4):
                     ny = y+dy[dir]; nx = x+dx[dir]
                     if ny < 0 or nx < 0 or ny >= n or nx >= n: continue
-                    nd = dp[y][x]+(a[ny][nx] == ['a', 'b'][type^(now&1)]); 
-                    if a[ny][nx] == ['b', 'a'][type] and not now: nd = 2
+                    nd = dp[y][x]+(a[ny][nx] == ['a', 'b'][type^(res&1)]) 
+                    if a[ny][nx] == ['b', 'a'][type] and not res: nd = 2
                     if dp[ny][nx] <= nd: continue
-                    dp[ny][nx] = nd; dq[nd-now].append((ny, nx))
-        if dp[by][bx] > 1:return inf
-        return now
+                    dp[ny][nx] = nd; dq[nd%3].append((ny, nx))
+        if dp[by][bx] > 1: return inf
+        return res
     ans = inf; ans = min(ans, solved(start, end, 0), solved(end, start, 1))
     if ans == inf: print(-1)
     else: print(ans)
