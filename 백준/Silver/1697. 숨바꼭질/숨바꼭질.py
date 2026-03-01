f086d6b790ddf9d08fe913668115698a6f10c1b3
Godel99 <tylor9632@gmail.com>
2026-03-01 12:43:54 +0900

    [Silver I] Title: 숨바꼭질, Time: 96 ms, Memory: 37836 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,22 @@
+import sys
+print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+
+from collections import deque
+
+def main():
+    n, k = map(int, input().split())
+    if n == k: print(0); return
+    maxl = (n if n > k else k)+2
+    dp = [0]*maxl
+    dq = deque([n])
+    while dq:
+        cur = dq.popleft()
+        for dir in [-1, 1, cur]:
+            nxt = cur+dir
+            if 0 <= nxt < maxl and not dp[nxt]:
+                dp[nxt] = dp[cur]+1
+                dq.append(nxt)
+    print(dp[k])
+if __name__ == '__main__':
+    main()
