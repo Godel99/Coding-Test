6e33d3d182a231f4947ac3fd3d85dbc297a9d7e5
Godel99 <tylor9632@gmail.com>
2026-03-03 21:13:53 +0900

    [Platinum V] Title: 최대공약수 완충재, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/34967. 최대공약수 완충재/최대공약수 완충재.cc
+++ 백준/Platinum/34967. 최대공약수 완충재/최대공약수 완충재.cc
@@ -4,19 +4,14 @@ using ll = long long;
 
 int main(){
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
-    int i, j; i = 3, j = 14001;
-    vector<int> res;
-    while(i != j){
-        res.push_back(i*j);
-        res.push_back(i*j);
-        if(i+j == 14004) j -= 2;
-        else i += 2;
+    int n; cin >> n;
+    int i = 1, j = 19999;
+    for(int k = 0; k < n-1; k++){
+        cout << i*j << ' ';
+        if(k&1) i++;
+        else j--;
     }
-    res.pop_back();
-    int n, m = res.size(); cin >> n;
-    for(int k = 0; k < n; k++){
-        if(k < m) cout << res[k]*2 << ' ';
-        else cout << res[m*2-1-k] << ' ';
-    }
+    if(n < 20000) cout << i*j;
+    else cout << 10000000;
     return 0;
 }
