93ab23126bc269c26700904a9e9a1c24c3d14096
Godel99 <tylor9632@gmail.com>
2026-03-04 15:22:38 +0900

    [Gold V] Title: Z, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.py
+++ 백준/Gold/1074. Z/Z.py
@@ -4,15 +4,10 @@ input = lambda: sys.stdin.readline().rstrip('\r\n')
 
 def main():
     n, r, c = map(int, input().split())
-    def solved(n, r, c):
-        if n == 0: return 0
-        h = 1<<(n-1)
-        size = h*h
-        if r < h and c < h: return solved(n-1, r, c)
-        elif r < h and c >= h: return size+solved(n-1, r, c-h)
-        elif r >= h and c < h: return 2*size+solved(n-1, r-h, c)
-        else: return 3*size+solved(n-1, r-h, c-h)
-    print(solved(n, r, c))
-    return 0
+    ans = 0
+    for i in range(n):
+        ans |= (c&(1<<i))<<i
+        ans |= (r&(1<<i))<<(i+1)
+    print(ans)
 if __name__ == '__main__':
     main()
