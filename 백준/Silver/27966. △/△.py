098e7da8ed5f91718f584fb4f1d1b93ae56102ed
Godel99 <tylor9632@gmail.com>
2026-03-08 22:56:20 +0900

    [Silver III] Title: △, Time: 124 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/27966. △/△.py
+++ 백준/Silver/27966. △/△.py
@@ -1,12 +1,10 @@
-import sys
-def print(*args, sep=" ", end="\n"):
-    sys.stdout.write(sep.join(map(str, args)) + end)
-def input():
-    return sys.stdin.readline().rstrip()
-    
+import sys 
+print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+
 def main():
     n = int(input())
-    print(pow(n-1,2))
+    print((n-1)*(n-1))
     for i in range(2, n+1): print(1, i)
 if __name__ == '__main__':
     main()
