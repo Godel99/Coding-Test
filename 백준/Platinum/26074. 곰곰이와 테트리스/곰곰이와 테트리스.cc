1fa38cbeae6fa75d1987ccc3d106c6b215d0684a
Godel99 <tylor9632@gmail.com>
2026-03-11 23:03:19 +0900

    [Platinum V] Title: 곰곰이와 테트리스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/26074. 곰곰이와 테트리스/곰곰이와 테트리스.cc
+++ 백준/Platinum/26074. 곰곰이와 테트리스/곰곰이와 테트리스.cc
@@ -5,6 +5,6 @@ using ll = long long;
 int main(){
     cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
     int n, m; cin >> n >> m;
-    cout << (n == 1 && m == 2 || n == 2 && m == 1 ? "ChongChong" : "GomGom");
+    cout << (n*m == 2 ? "ChongChong" : "GomGom");
     return 0;
 }
