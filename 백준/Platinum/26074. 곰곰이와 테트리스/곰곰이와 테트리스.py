5c4360b4d5a6ad8a344eff77c43e17a874b985bf
Godel99 <tylor9632@gmail.com>
2026-03-11 23:01:50 +0900

    [Platinum V] Title: 곰곰이와 테트리스, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Platinum/26074. 곰곰이와 테트리스/곰곰이와 테트리스.py
+++ 백준/Platinum/26074. 곰곰이와 테트리스/곰곰이와 테트리스.py
@@ -4,6 +4,6 @@ input = lambda: sys.stdin.readline().rstrip('\r\n')
 
 def main():
     n, m = map(int, input().split())
-    print('ChongChong' if n == 1 and m == 2 or n == 2 and m == 1 else 'GomGom')
+    print('ChongChong' if n*m == 2 else 'GomGom')
 if __name__ == '__main__':
     main()
