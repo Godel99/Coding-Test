cf4ee134461980a11a1f7ef56223882d373ded52
Godel99 <tylor9632@gmail.com>
2026-03-11 15:10:56 +0900

    [Silver III] Title: N과 M (4), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,18 @@
+import sys 
+print = lambda *args, sep=" ", end="\n": sys.stdout.write(sep.join(map(str, args)) + end)
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+
+def main():
+    n, m = map(int, input().split())
+    ans = []
+    def dfs(x, d):
+        if d == m:
+            print(*ans)
+            return
+        for i in range(x, n+1):
+            ans.append(i)
+            dfs(i, d+1)
+            ans.pop()
+    dfs(1, 0)
+if __name__ == '__main__':
+    main()
