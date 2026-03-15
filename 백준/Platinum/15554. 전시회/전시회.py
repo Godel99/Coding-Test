b5e01a9ef5c91729af87f59d70a736f2dbccea6d
Godel99 <tylor9632@gmail.com>
2026-03-15 13:07:15 +0900

    [Platinum V] Title: 전시회, Time: 1788 ms, Memory: 136776 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/15554. 전시회/전시회.py
+++ 백준/Platinum/15554. 전시회/전시회.py
@@ -8,12 +8,11 @@ def main():
     n = int(input())
     a = [list(map(int, input().split())) for _ in range(n)]
     a.sort()
-    L, R, pre= inf, 0, 0
+    L, R, pres = inf, 0, 0
     for i in range(n):
-        pres = pre+a[i][1]
-        L = min(L, pre-a[i][0])
-        R = max(R, pres-a[i][0]-L)
-        pre = pres
+        L = min(L, pres-a[i][0])
+        R = max(R, pres+a[i][1]-a[i][0]-L)
+        pres += a[i][1]
     print(R)
 if __name__ == '__main__':
     main()
