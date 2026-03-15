2173d30f883d5988b9c97bf3a8c7b9d0e3b35979
Godel99 <tylor9632@gmail.com>
2026-03-15 13:12:16 +0900

    [Platinum V] Title: 전시회, Time: 1580 ms, Memory: 169000 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/15554. 전시회/전시회.py
+++ 백준/Platinum/15554. 전시회/전시회.py
@@ -11,8 +11,8 @@ def main():
     L, R, pres = inf, 0, 0
     for i in range(n):
         L = min(L, pres-a[i][0])
-        R = max(R, pres+a[i][1]-a[i][0]-L)
         pres += a[i][1]
+        R = max(R, pres-a[i][0]-L)
     print(R)
 if __name__ == '__main__':
     main()
